32c2afa4e1ce0aadaa8de00a66d335e6fe5488af
callmekund <gauravbanna5555@gmail.com>
2025-05-02 10:36:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://sweet-haily-gauravbanna-2e1f627a.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
